5e1736fadb712f4040e9a79256527bbda67041f1
Jesse Grobstein <60900281+jgrobstein@users.noreply.github.com>
2022-03-06 14:06:26 -0500

    Added update-json module.

---
+++ update-json.mjs
@@ -0,0 +1,32 @@
+import process from 'process';
+import minimist from 'minimist';
+import replace from 'replace-in-file';
+
+async function main () {
+    const args = minimist(process.argv.slice(2));
+    const cid = args.cid;
+
+    if (!cid) {
+        return console.error('Please provide a CID.')
+    }
+
+    if (args._.length < 1) {
+        return console.error('Please supply the path to a directory')
+    }
+
+    const path = args._[0].concat("*.", "json");
+
+    const options = {
+        files: path,
+        from: /<CID>/g,
+        to: cid,
+    };
+
+    replace(options)
+    .then(console.log('Successfully completed.'))
+    .catch(error => {
+        console.error('Error occurred:', error);
+    });
+}
+
+main();
